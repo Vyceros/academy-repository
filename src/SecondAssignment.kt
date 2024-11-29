dfaf9e9b620094be162fa42141efc51f6724606f
Vyceros <luka.kurashvili.1@btu.edu.ge>
2024-11-29 22:32:52 +0400

    usj

--- src/SecondAssignment.kt
+++ src/SecondAssignment.kt
@@ -3,6 +3,7 @@ fun main() {
     val mathematics = MathematicsOperations()
 
     println(mathematics.greatCommonDivisor(10,3))
+    println(mathematics.leastCommonFactor(12,15))
 }
 
 class MathematicsOperations()
@@ -16,6 +17,25 @@ class MathematicsOperations()
         }else
         {
             return greatCommonDivisor(intTwo, intOne % intTwo)
+        }
+
+    }
+    fun leastCommonFactor(intOne : Int, intTwo : Int) : Int
+    {
+        return (intOne * intTwo) / greatCommonDivisor(intOne,intTwo)
+    }
+    fun leastCommonFactor2(intOne : Int, intTwo : Int) : Int
+    {
+        val biggerNumber = maxOf(intOne,intTwo)
+        var leastCommon = biggerNumber
+
+        while(true)
+        {
+            if (leastCommon % intOne == 0 && leastCommon % intTwo ==0)
+            {
+                return leastCommon
+            }
+            leastCommon++
         }
 
     }
