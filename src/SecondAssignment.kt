d9abbc5c4d851980a32a77bcf6c3d03f0a526e62
Vyceros <luka.kurashvili.1@btu.edu.ge>
2024-11-30 11:42:04 +0400

    final

--- src/SecondAssignment.kt
+++ src/SecondAssignment.kt
@@ -4,11 +4,16 @@ fun main() {
 
     println(mathematics.greatCommonDivisor(10,3))
     println(mathematics.leastCommonFactor(12,15))
+    println(mathematics.containsDollarSign("ei1321#$$"))
+    println(mathematics.recursiveEven(100))
+    val flipped = mathematics.flipNumber(12)
+    print(flipped)
+    println(mathematics.findPalindrome(122))
+
 }
 
 class MathematicsOperations()
 {
-    // e.g 12 and 8 returns 4
     fun greatCommonDivisor(intOne : Int, intTwo : Int) : Int
     {
         if(intTwo == 0)
@@ -38,5 +43,44 @@ class MathematicsOperations()
             leastCommon++
         }
 
+    }
+    fun containsDollarSign(input : String) : Boolean
+    {
+        return input.contains('$')
+    }
+    fun recursiveEven(ceiling : Int, number : Int = 0, sum : Int = 0) : Int{
+        if(number >= ceiling)
+        {
+            return sum
+        }else if(number % 2 == 0)
+        {
+            return recursiveEven(ceiling,number + 1,sum + number
+            )
+        }else
+        {
+            return recursiveEven(ceiling,number + 1,sum)
+        }
+
+    }
+    fun flipNumber(number : Int) : Int
+    {
+        var tempDigit = 0
+        var tempNumber = number
+        var reversed = 0
+        while(tempNumber != 0)
+        {
+
+            tempDigit = tempNumber % 10
+
+            reversed = reversed * 10 + tempDigit
+
+            tempNumber /= 10
+
+        }
+        return reversed
+    }
+    fun findPalindrome(number : Int) : Boolean
+    {
+        return number == flipNumber(number)
     }
 }
