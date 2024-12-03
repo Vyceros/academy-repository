d00b732897c0d9257932f128cb5d4d2f6fb6b545
Vyceros <luka.kurashvili.1@btu.edu.ge>
2024-12-03 19:34:20 +0400

    completed converter

--- app/src/main/java/com/example/learnapplication/MainActivity.kt
+++ app/src/main/java/com/example/learnapplication/MainActivity.kt
@@ -1,17 +1,40 @@
 package com.example.learnapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+
+val singleDigits = NumberArraysGeorgian().singleDigit
+val twoDigits = NumberArraysGeorgian().twoDigits
+val tenMultiples = NumberArraysGeorgian().tenMultiples
+val tenCompounds = NumberArraysGeorgian().tensPlaceCompound
+val threeDigits = NumberArraysGeorgian().threeDigits
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.etInputField)
+        val btnConfirmation: Button = findViewById(R.id.btnConverter)
+        val textResultView: TextView = findViewById(R.id.tvResultText)
+
+        btnConfirmation.setOnClickListener {
+            val converter = ConvertToGeorgian()
+            val stringRepresent = editText.text.toString()
+            textResultView.text = converter.convertToGeorgian(stringRepresent.toInt())
+        }
 
     }
     //nullable shemdeg leqciaze, collections
 }
+
+
+
+
+
+
