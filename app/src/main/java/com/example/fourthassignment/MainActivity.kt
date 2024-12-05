a9909f4ec4a9a502d7792ec2babd81de9afcee71
Vyceros <luka.kurashvili.1@btu.edu.ge>
2024-12-05 22:31:16 +0400

    wip

--- app/src/main/java/com/example/fourthassignment/MainActivity.kt
+++ app/src/main/java/com/example/fourthassignment/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.fourthassignment
-
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class MainActivity : AppCompatActivity() {
+    //Buttons
+    private lateinit var btnSave : Button
+    private lateinit var btnClear : Button
+    private lateinit var btnAgain : Button
+
+    //Inputs
+    private lateinit var etUsername : EditText
+    private lateinit var etEmail : EditText
+    private lateinit var etFirstName : EditText
+    private lateinit var etLastName : EditText
+    private lateinit var etAge : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
     }
 
