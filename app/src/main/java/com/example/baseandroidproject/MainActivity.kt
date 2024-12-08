315b41ac8fc9e4b61e1e2a78e2a57c100c44b4bd
Vyceros <luka.kurashvili.1@btu.edu.ge>
2024-12-08 14:55:33 +0400

    Design changes, user class added

--- app/src/main/java/com/example/baseandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/baseandroidproject/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.baseandroidproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.SearchView
+import com.example.baseandroidproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
     }
 }
