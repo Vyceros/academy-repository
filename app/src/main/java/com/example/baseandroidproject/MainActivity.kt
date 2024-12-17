5717c6ec8d3647fa06c2887cbbde41969629b4fe
Vyceros <luka.kurashvili.1@btu.edu.ge>
2024-12-17 18:27:52 +0400

    Setting up user and user storage classes, set up layout.

--- app/src/main/java/com/example/baseandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/baseandroidproject/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.baseandroidproject
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.baseandroidproject.UserInfo.User
+import com.example.baseandroidproject.UserInfo.UserStorage
+import com.example.baseandroidproject.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var user: User
+    private lateinit var userStorage: UserStorage
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    private fun onAddUserButton() {
+        binding.btnAddUser.setOnClickListener {
+            userStorage = UserStorage()
+            user = User(
+                firstName = binding.etFirstName.text.toString(),
+                lastName = binding.etLastName.text.toString(),
+                email = binding.etEmail.text.toString(),
+                age = binding.etAge.text.toString()
+
+            )
+            userStorage.addUser(user)
         }
     }
 }
