81f6c3f9a5dd4afddbf4a691e8558b86b376ba73
Vyceros <luka.kurashvili.1@btu.edu.ge>
2025-01-31 08:35:38 +0400

    rqacaca

---
+++ app/src/main/java/com/example/baseandroidproject/generics/Generics.kt
@@ -0,0 +1,212 @@
+package com.example.baseandroidproject.generics
+
+
+////region WHY DO WE NEED THEM
+//
+//fun main() {
+//    /**
+//     *NOT TYPE SAFE
+//     * TYPE IS NOT ENFORCED AT COMPILE TIME.
+//     * DOESNT GIV ERRORS HERE BUT WILL THROW AN EXCEPTION IN RUNTIME
+//     * **/
+//
+//    val anyBox = AnyBox()
+//    anyBox.setValue(1)
+//    val anyValue = anyBox.getValue()
+//
+//    /**
+//     * TYPE SAFE
+//     * TYPE IS STRICTLY ENFORCED AT COMPILE TIME
+//     * NO ROOM FOR ERROR IN RUNTIME
+//     * **/
+//    val genericBox = Box<Int>()
+//    genericBox.setValue(1)
+//    val genericValue = genericBox.getValue()
+//
+////No need for type casting
+//
+//
+//}
+//
+//class Box<T>() {
+//    private var value: T? = null
+//
+//    fun setValue(value: T) {
+//        this.value = value
+//    }
+//
+//    fun getValue(): T? {
+//        return value
+//    }
+//}
+//
+//class AnyBox() {
+//    private var value: Any? = null
+//
+//    fun setValue(value: Any) {
+//        this.value = value
+//    }
+//
+//    fun getValue(): Any? {
+//        return value
+//    }
+//}
+//
+////endregion
+
+////region interfaces
+//fun main() {
+//    CarFactory().apply {
+//        this.getById(1).also { println(it) }
+//        this.getAll().also { println(it) }
+//    }
+//
+//}
+//
+//interface Factory<T> {
+//    fun getById(id: Int): T
+//    fun getAll(): List<T>
+//}
+//
+//class CarFactory : Factory<Car> {
+//    private val cars = listOf(Car(1, "Toyota"))
+//
+//    override fun getById(id: Int): Car {
+//        return cars.first { it.id == id }
+//    }
+//
+//    override fun getAll(): List<Car> {
+//        return cars
+//    }
+//}
+//
+//data class Car(val id: Int, val name: String)
+//
+//
+////endregion
+
+////region methods
+//
+//fun main() {
+//    val result = multipleTypeParams("Hello World") { it.substring(0, 5) }
+//    println(result)
+//    listOf(1,2,3).also { printList(it) }
+//    listOf("One","Two","Three").also { printList(it) }
+//}
+//
+//fun <T, R> multipleTypeParams(item: T, action: (T) -> R): R {
+//    return action(item)
+//}
+//
+//fun <T> printList(list: List<T>) {
+//    list.forEach { println(it) }
+//}
+//
+////endregion
+
+////region type constraint
+//
+//fun main() {
+//    printMax(listOf(1, 2, 3))
+//    printMax(listOf("One", "Two", "Three"))
+//    println(compareNumbers(1,5))
+//}
+//
+////upper bound
+//fun <T : Comparable<T>> printMax(list: List<T>) {
+//    val max = list.maxOrNull()
+//    println(max)
+//}
+//
+////multiple upper bounds with WHERE
+//fun <T> compareNumbers(first : T, second : T) : Boolean
+// where T : Number, T : Comparable<T> {
+//     return first > second
+// }
+//
+//
+////endregion
+
+////region variance
+//
+//fun main(){
+//    val outClass = OutClass("Im out classs")
+//    val refToOut : OutClass<Any> = outClass
+//
+//    val inClass = InClass<Number>()
+//    val refToIn : InClass<Double> = inClass
+//
+//
+//    //val invariantGeneric = GenericClass("Im a string")
+//    //val refToInvariant : GenericClass<Any> = invariantGeneric
+//}
+//
+////region In Contravariance
+//
+////in - consumer of T, can only consume the value of T but not return it
+//class InClass< in T>{
+//    fun convertToString(item: T) : String{
+//        return item.toString()
+//    }
+//}
+//
+//
+//// endregion
+//
+//
+////for read operations only
+////when we dont know expected type or we dont care about it.
+//fun printListSafe(list: List<*>) {
+//    list.forEach { println(it) }
+//}
+//
+////region Invariance
+//
+//class GenericClass<T>(val value : T){
+//    fun foo() : T{
+//        return value
+//    }
+//}
+//
+//
+//// endregion
+//
+////region Out Covariance
+//    //out - producer of T, can only produce/return the value of T but not consume it
+//    class OutClass<out T>(val value : T){
+//        fun get() : T{
+//            return value
+//        }
+//    }
+//
+//
+////endregion
+
+//region reified
+
+fun main() {
+    val intList : List<Int> = listOf(1,2,3)
+    val stringList : List<String> = listOf("one","two","tthree")
+    checkTypeAtRuntime<String>(stringList)
+
+    //type erased at runtime, List<Int> becomes just ArrayList
+    println(intList.javaClass)
+}
+
+inline fun <reified T> checkTypeAtRuntime(list: List<T>){
+    if (T::class == String::class){
+        println("List is of type String")
+    }
+}
+
+//when do we use it?
+
+
+
+//endregion
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/baseandroidproject/generics/ReifiedUsecase.kt
@@ -0,0 +1,142 @@
+package com.example.baseandroidproject.generics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+
+//region usecase1
+
+//same as box analogy, to wrap the common logic in a single place
+
+class Resource<T> {
+    private var data: T? = null
+    private var error: String? = null
+    private var isLoading: Boolean = false
+
+    fun setSuccess(value: T) {
+        data = value
+        error = null
+        isLoading = false
+    }
+
+    fun setError(message: String) {
+        data = null
+        error = message
+        isLoading = false
+    }
+
+    fun setLoading() {
+        isLoading = true
+    }
+}
+
+
+// endregion
+
+//region usecase2
+
+//repository patterns/CRUD operations
+//generic repository interface that works with any data type
+interface Repository<T> {
+    suspend fun fetch(id: String): T?
+    suspend fun save(item: T)
+    suspend fun delete(id: String)
+    suspend fun getAll(): List<T>
+}
+
+
+//endregion
+
+//region usecase 3
+class Analytics {
+    //log events of any type in runtime
+    inline fun <reified T> logEvent(event: T) {
+        println(T::class.simpleName ?: "Unknown")
+    }
+}
+
+//endregion
+
+//region base class
+
+
+abstract class BaseFragment<VB : ViewBinding>(
+    private val inflateBinding: (LayoutInflater, ViewGroup?, Boolean) -> VB
+) : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflateBinding(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
+//endregion
+
+//region vmm
+
+class ViewModelFactory<VM : ViewModel>(
+    private val viewModelClass: Class<VM>,
+    private val creator: () -> VM
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(viewModelClass)) {
+
+            return creator() as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: ${viewModelClass.name}")
+    }
+}
+
+//endregion
+
+//region mapper
+//I - what we map from O- what we map to
+interface Mapper<I, O> {
+    fun map(input: I): O
+}
+
+
+data class UserEntity(val id: String, val fullName: String)
+data class UserDTO(val id: String, val nameModified: String)
+
+class UserMapper : Mapper<UserEntity, UserDTO> {
+    override fun map(input: UserEntity): UserDTO {
+        return UserDTO(input.id, input.fullName.substring(0,5))
+    }
+}
+
+// Usage
+val userEntity = UserEntity("111", "luka kurashvili")
+val userDTO = UserMapper().map(userEntity)
+
+fun main(){
+    println(userDTO)
+    val mixedList = listOf(1,5.3,"racxa","kotlini",'c')
+
+    val stringList = filterByType<String>(mixedList)
+    println(stringList)
+}
+//endregion
+
+inline fun <reified T> filterByType(list: List<Any>): List<T> {
+
+    return list.filterIsInstance<T>()
+
+}
