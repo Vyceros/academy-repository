1cbf378fb5e38a812f2b15733201d43030a7bffe
Vyceros <luka.kurashvili.1@btu.edu.ge>
2025-03-25 22:44:33 +0400

    Broadcasts.

---
+++ app/src/main/java/com/example/baseandroidproject/AirplaneModeReceiver.kt
@@ -0,0 +1,22 @@
+package com.example.baseandroidproject
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+
+//Dynamic receiver
+class AirplaneModeReceiver(private val callback : (Boolean) -> Unit) : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val isAirplaneModeEnabled = intent?.getBooleanExtra("state", false) ?: return
+        if (isAirplaneModeEnabled) {
+            callback(true)
+            Toast.makeText(context, "Airplane Mode Enabled", Toast.LENGTH_LONG).show()
+        } else {
+            callback(false)
+            Toast.makeText(context, "Airplane Mode Disabled", Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
--- app/src/main/java/com/example/baseandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/baseandroidproject/MainActivity.kt
@@ -1,27 +1,32 @@
 package com.example.baseandroidproject
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.example.baseandroidproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver : AirplaneModeReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        handleEdgeToEdge()
         setContentView(binding.root)
+        registerReceiver()
     }
 
-    private fun handleEdgeToEdge(){
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    private fun registerReceiver(){
+        receiver = AirplaneModeReceiver{
+            binding.loader.isVisible = it
         }
+        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(receiver,intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
